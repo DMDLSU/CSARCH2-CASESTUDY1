95a9c3ded67aaf6163b9e3b6d0cb47a0873f70cd
AndreiXIV <alessandro_encarnacion@dlsu.edu.ph>
2026-08-04 15:03:33 +0800

    Added Comments

--- converter.js
+++ converter.js
@@ -1,5 +1,6 @@
+//Function for converting to unsigned binary
 function unsigned(number, bits) {
-    let max = (2 ** bits) -1;
+    let max = (2 ** bits) -1; //determines max possible bit for given number
 
     if (number < 0) {
         return "Error: Unsigned numbers cannot be negative";
@@ -9,25 +10,26 @@ function unsigned(number, bits) {
         return "Error: Number is too large to fit in selected bits";
     }
 
-    let binary = number.toString(2);
+    let binary = number.toString(2); //converts int to binary string
 
     while (binary.length < bits) {
-        binary = "0" + binary;
+        binary = "0" + binary; //For zero padding
     }
 
     return binary;
 
 }
 
+//Function for convertin to signed binary
 function signed(number, bits) {
 
+    //Smallest and Biggest possible number that can be represented
     let min = -(2 ** (bits - 1));
     let max = (2 ** (bits - 1)) - 1;
 
        if (number < min) {
         return "Error: Number is too small to fit in selected bits";
     }
-
         if (number > max) {
         return "Error: Number is too large to fit in selected bits";
     }
@@ -36,17 +38,17 @@ function signed(number, bits) {
 
     
     if (number >= 0) {
-        binary = number.toString(2);
+        binary = number.toString(2); 
         while (binary.length < bits) {
             binary = "0" + binary;
         }
-
     }
     
     else {
 
+        //Gets 2's compement to get negative
         let value = (2 ** bits) + number;
-        binary = value.toString(2);
+        binary = value.toString(2); 
         while (binary.length < bits) {
             binary = "0" + binary;
         }
